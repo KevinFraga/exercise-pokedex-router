672a24805c07cc36edf38126d0084709fffa4884
Kevin Fraga <kevinciolac@gmail.com>
2021-03-31 18:31:58 -0300

    ex. 2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 import NotFound from './notFound';
+import PokemonDetails from './PokemonDetails';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <h1> Pokedex </h1>
       <Switch>
         <Route exact path="/" component={ () => <Pokedex pokemons={pokemons} /> } />
+        <Route path="/pokemons/:id" component={ PokemonDetails } />
         <Route path="/" component={ NotFound } />
       </Switch>
     </div>
---
+++ src/PokemonDetails.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PokemonDetails() {
+  return (
+    <p>Teste</p>
+  );
+}
+
+export default PokemonDetails;
