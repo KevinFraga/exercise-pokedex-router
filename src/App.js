ce4ea64bf9311135bb41a41299ac7c9ecac25b0b
Kevin Fraga <kevinciolac@gmail.com>
2021-04-01 17:54:30 -0300

    ex. 4, 5, 6

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import pokemons from './data';
 import Pokedex from './Pokedex';
 import NotFound from './notFound';
 import PokemonDetails from './PokemonDetails';
+import About from './About';
 
 function App() {
   return (
     <div className="App">
       <h1> Pokedex </h1>
       <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
       <Switch>
         <Route exact path="/" component={ () => <Pokedex pokemons={pokemons} /> } />
         <Route path="/pokemons/:id" component={ PokemonDetails } />
+        <Route path="/about" component={ About } />
         <Route path="/" component={ NotFound } />
       </Switch>
     </div>
